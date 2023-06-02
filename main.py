59f4d4e56bdd85f0738b5651d2040597318f3774
Chesnokov <henryycampbell@gmail.com>
2023-06-02 07:08:47 +0300

    add finite_differences & TODO: fix get_finite_differences_table(dots)

--- main.py
+++ main.py
@@ -5,42 +5,45 @@ import datetime
 import function
 import interpolation as interpol  # xD
 
-from interpolation import get_finite_differences_table
+# from interpolation import get_finite_differences_table
 
-# dots = [[2.1, 3.759], [2.15, 4.186], [2.2, 4.922], [2.25, 5.349], [2.3, 5.928], [2.35, 6.419], [2.4, 7.084]]
+dots = [[0.1, 1.25], [0.2, 2.38], [0.3, 3.79], [0.4, 5.44], [0.5, 7.14]]
 # table = get_finite_differences_table(dots)
 # print(table)
 
-def print_table(table):
-    for node in table:
-        print(node + " ")
+# def print_table(table):
+#     for node in table:
+#         print(node + " ")
+#
+#
+# FILENAME: Final[str] = 'test.txt'
+#
+# print('Вас приветствует программа "интерполяция функции"! Текущее время: ' + str(datetime.datetime.now()))
+# print('Выберите тип ввода функции: ')
+# print('1. Файл')
+# print('2. Клавиатура')
+# print('3. Список функций')
+#
+# input_type = int(input()) # TODO сделать рефакторинг
+# match input_type:
+#     case 1:
+#         print('Выбранный тип ввода: файл')
+#         table = input.get_table_from_file(FILENAME)
+#         print_table(table)
+#     case 2:
+#         print('Выбранный тип ввода: клавиатура')
+#         table = input.get_table_from_keyboard()
+#         print_table(table)
+#     case 3:
+#         print('Выбранный тип ввода: список функций')
+#         function.print_functions()
+#         print('Введите номер функции: ')
+#         function_number = int(input())
+#         function = function.choose_function(function_number)
+#
+# # TODO подсчет с помощью Лагранжа и Гаусса, сравнение, графики
 
-
-FILENAME: Final[str] = 'test.txt'
-
-print('Вас приветствует программа "интерполяция функции"! Текущее время: ' + str(datetime.datetime.now()))
-print('Выберите тип ввода функции: ')
-print('1. Файл')
-print('2. Клавиатура')
-print('3. Список функций')
-
-input_type = int(input()) # TODO сделать рефакторинг
-match input_type:
-    case 1:
-        print('Выбранный тип ввода: файл')
-        table = input.get_table_from_file(FILENAME)
-        print_table(table)
-    case 2:
-        print('Выбранный тип ввода: клавиатура')
-        table = input.get_table_from_keyboard()
-        print_table(table)
-    case 3:
-        print('Выбранный тип ввода: список функций')
-        function.print_functions()
-        print('Введите номер функции: ')
-        function_number = int(input())
-        function = function.choose_function(function_number)
-
-# TODO подсчет с помощью Лагранжа и Гаусса, сравнение, графики
+print(interpol.gauss_first_formula(dots, 0.32))
+print(interpol.gauss_second_formula(dots, 0.28))
 
 
