348ed86ec3efb5913ebbe10f93c565a4a5497715
Chesnokov <henryycampbell@gmail.com>
2023-06-02 09:02:23 +0300

    some fix

--- main.py
+++ main.py
@@ -8,38 +8,38 @@ import interpolation as interpol  # xD
 # from interpolation import get_finite_differences_table
 
 dots = [[0.1, 1.25], [0.2, 2.38], [0.3, 3.79], [0.4, 5.44], [0.5, 7.14]]
-# table = get_finite_differences_table(dots)
+# table = get_finite_differences_table(dots) FIXME
 # print(table)
 
-# def print_table(table):
-#     for node in table:
-#         print(node + " ")
-#
-#
-# FILENAME: Final[str] = 'test.txt'
-#
-# print('Вас приветствует программа "интерполяция функции"! Текущее время: ' + str(datetime.datetime.now()))
-# print('Выберите тип ввода функции: ')
-# print('1. Файл')
-# print('2. Клавиатура')
-# print('3. Список функций')
-#
-# input_type = int(input()) # TODO сделать рефакторинг
-# match input_type:
-#     case 1:
-#         print('Выбранный тип ввода: файл')
-#         table = input.get_table_from_file(FILENAME)
-#         print_table(table)
-#     case 2:
-#         print('Выбранный тип ввода: клавиатура')
-#         table = input.get_table_from_keyboard()
-#         print_table(table)
-#     case 3:
-#         print('Выбранный тип ввода: список функций')
-#         function.print_functions()
-#         print('Введите номер функции: ')
-#         function_number = int(input())
-#         function = function.choose_function(function_number)
+def print_table(table):
+    for node in table:
+        print(node + " ")
+
+
+FILENAME: Final[str] = 'test.txt'
+
+print('Вас приветствует программа "интерполяция функции"! Текущее время: ' + str(datetime.datetime.now()))
+print('Выберите тип ввода функции: ')
+print('1. Файл')
+print('2. Клавиатура')
+print('3. Список функций')
+
+input_type = int(input()) # TODO сделать рефакторинг
+match input_type:
+    case 1:
+        print('Выбранный тип ввода: файл')
+        table = input.get_table_from_file(FILENAME)
+        print_table(table)
+    case 2:
+        print('Выбранный тип ввода: клавиатура')
+        table = input.get_table_from_keyboard()
+        print_table(table)
+    case 3:
+        print('Выбранный тип ввода: список функций')
+        function.print_functions()
+        print('Введите номер функции: ')
+        function_number = int(input())
+        function = function.choose_function(function_number)
 #
 # # TODO подсчет с помощью Лагранжа и Гаусса, сравнение, графики
 
